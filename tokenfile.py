d118c82620949c9253bafabd6c98df96ea3c6cf7
ashik <ashikagumbe@gmail.com>
2022-05-26 20:19:13 +0530

    removed catch file

--- tokenfile.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-tokenizer = AutoTokenizer.from_pretrained("lidiya/bart-large-xsum-samsum")
-
-model = AutoModelForSeq2SeqLM.from_pretrained("lidiya/bart-large-xsum-samsum")
